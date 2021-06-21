56b771c62ec1133d54f3eed4c76fb3ff850586e1
Raval <jayrava@38f9d3641d1b.ant.amazon.com>
2021-06-21 09:41:21 -0400

    modified config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //return <Component {...pageProps} />
+  return <h1>The value of test is: {process.env.test}</h1>
 }
 
 export default MyApp
