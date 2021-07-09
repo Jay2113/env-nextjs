357219db8a2ec292b10ffd2e41eb1a6d2c261101
Raval <jayrava@38f9d3641d1b.ant.amazon.com>
2021-07-09 17:19:33 -0400

    adding amplify

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Amplify from "aws-amplify"
 
 export default function Home() {
   return (
